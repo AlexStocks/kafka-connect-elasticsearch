23c4e4aac7752312b0fdff7f9a7d3c815aec062d
AlexStocks <alexstocks@foxmail.com>
2017-11-02 14:43:47 +0800

    bug fix: log type should be string

--- app/worker.go
+++ app/worker.go
@@ -98,7 +98,7 @@ LOOP:
 		if flag {
 			err = EsClient.BulkInsert(getIndex(), Kafka2EsConf.Es.Type, docArray)
 			if err != nil {
-				Log.Error("error:%#v, log:%#v", err, docArray[0])
+				Log.Error("error:%#v, log:%s", err, string(docArray[0].([]byte)))
 			} else {
 				Log.Info("successfully insert %d msgs into es", len(docArray))
 			}
